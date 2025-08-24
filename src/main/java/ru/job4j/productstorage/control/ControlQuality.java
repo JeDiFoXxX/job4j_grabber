1b74e7c25e94c407aa1afe1eac373655a2b80568
JeDiFoXxX <serjik1993991@gmail.com>
2025-08-24 18:38:26 +0300

    Динамическое перераспределение продуктов

--- src/main/java/ru/job4j/productstorage/control/ControlQuality.java
+++ src/main/java/ru/job4j/productstorage/control/ControlQuality.java
@@ -3,6 +3,7 @@ package ru.job4j.productstorage.control;
 import ru.job4j.productstorage.model.Food;
 import ru.job4j.productstorage.store.AbstractStore;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ControlQuality {
@@ -15,6 +16,19 @@ public class ControlQuality {
     }
 
     public void controlQuality() {
+        sort(this.foods);
+    }
+
+    public void resort() {
+        List<Food> foods = new ArrayList<>();
+        for (AbstractStore store : stores) {
+            foods.addAll(store.findAll());
+            store.findAll().clear();
+        }
+        sort(foods);
+    }
+
+    private void sort(List<Food> foods) {
         foods.forEach(food ->
                 stores.stream()
                         .filter(store -> store.check(food))
--- src/test/java/ru/job4j/productstorage/control/ControlQualityTest.java
+++ src/test/java/ru/job4j/productstorage/control/ControlQualityTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@Disabled
 class ControlQualityTest {
     private List<Food> foods;
     private List<AbstractStore> stores;
@@ -58,6 +57,7 @@ class ControlQualityTest {
         );
     }
 
+    @Disabled
     @Test
     void whenAppleThenGoesToShopStore() {
         List<Food> expected = List.of(new Fruit(
@@ -74,6 +74,7 @@ class ControlQualityTest {
         assertThat(stores.get(0).findAll()).containsExactlyInAnyOrderElementsOf(expected);
     }
 
+    @Disabled
     @Test
     void whenOrangeThenGoesToWarehouseStore() {
         List<Food> expected = List.of(new Fruit(
@@ -90,6 +91,7 @@ class ControlQualityTest {
         assertThat(stores.get(1).findAll()).containsExactlyInAnyOrderElementsOf(expected);
     }
 
+    @Disabled
     @Test
     void whenTomatoThenGoesToTrashStore() {
         List<Food> expected = List.of(new Vegetable(
@@ -104,5 +106,42 @@ class ControlQualityTest {
         ControlQuality control = new ControlQuality(stores, foods);
         control.controlQuality();
         assertThat(stores.get(2).findAll()).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void whenResortThenCorrectDistribution() {
+        Fruit apple = new Fruit(
+                "Apple",
+                "Russia",
+                LocalDateTime.of(2026, 8, 24, 0, 0, 0),
+                LocalDateTime.of(2025, 8, 24, 0, 0, 0),
+                100,
+                20
+        );
+        Fruit orange = new Fruit(
+                "Orange",
+                "Spain",
+                LocalDateTime.of(2025, 8, 23, 0, 0, 0),
+                LocalDateTime.of(2025, 8, 10, 0, 0, 0),
+                80,
+                15
+        );
+        Fruit banana = new Fruit(
+                "Banana",
+                "Ecuador",
+                LocalDateTime.of(2025, 9, 10, 0, 0, 0),
+                LocalDateTime.of(2025, 8, 10, 0, 0, 0),
+                90,
+                18
+        );
+        AbstractStore shop = new Shop(new ArrayList<>(List.of(apple)));
+        AbstractStore wareHouse = new Warehouse(new ArrayList<>(List.of(orange)));
+        AbstractStore trash = new Trash(new ArrayList<>(List.of(banana)));
+        this.stores = List.of(shop, wareHouse, trash);
+        ControlQuality control = new ControlQuality(stores, List.of());
+        control.resort();
+        assertThat(shop.findAll()).contains(banana);
+        assertThat(wareHouse.findAll()).contains(apple);
+        assertThat(trash.findAll()).contains(orange);
     }
 }
