fc0d0d804a87fcb4119f35040fc6dc377cec06e7
JeDiFoXxX <serjik1993991@gmail.com>
2025-09-06 13:27:02 +0300

    Рекурсивный вызов метода

---
+++ src/main/java/ru/job4j/recursion/Recursion.java
@@ -0,0 +1,63 @@
+package ru.job4j.recursion;
+
+public class Recursion {
+    public int sum(int summary, int index) {
+        if (index > 0) {
+            summary += index;
+            index--;
+            summary = sum(summary, index);
+        }
+        return summary;
+    }
+
+    public int loop(int summary, int index) {
+        for (int i = index; i > 0; i--) {
+            summary += i;
+        }
+        return summary;
+    }
+
+    public long factorialLoop(int f) {
+        long result = 1L;
+        if (f > 0) {
+            for (int i = 1; i <= f; i++) {
+                result = result * i;
+            }
+        }
+        return result;
+    }
+
+    public long factorialRecursion(long index) {
+        long result = index;
+        if (index > 0) {
+            result *= factorialRecursion(--index);
+        }
+        return result == 0 ? 1L : result;
+    }
+
+    public long fibonacciLoop(int n) {
+        long result = 0L;
+        if (n == 1) {
+            result = 1L;
+        } else if (n > 1) {
+            long f1 = 1L;
+            long f2 = 1L;
+            for (int i = 0; i < (n - 2); i++) {
+                result = f2 + f1;
+                f1 = f2;
+                f2 = result;
+            }
+        }
+        return result;
+    }
+
+    public long fibonacciRecursion(int n) {
+        long result = n;
+        if (n > 1) {
+            long f1 = fibonacciRecursion(n - 1);
+            long f2 = fibonacciRecursion(n - 2);
+            result = f1 + f2;
+        }
+        return result;
+    }
+}
