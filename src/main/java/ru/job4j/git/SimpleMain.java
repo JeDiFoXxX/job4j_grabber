8e054622cecc64640a0ccaa43c72d634ebdfcdc9
JeDiFoXxX <serjik1993991@gmail.com>
2025-10-21 10:49:45 +0300

    Create SimpleMain

---
+++ src/main/java/ru/job4j/git/SimpleMain.java
@@ -0,0 +1,11 @@
+package ru.job4j.git;
+
+public class SimpleMain {
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Hello World");
+    }
+}
